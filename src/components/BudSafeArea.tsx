399994593f9b7f63a0f3c32f555185e8ca52c111
Oskar Zdziarski <oskarzdz@gmail.com>
2025-03-26 12:53:18 -0700

    fix: get BudSafeArea from env

--- src/components/BudSafeArea.tsx
+++ src/components/BudSafeArea.tsx
@@ -4,11 +4,9 @@ interface Props {
   children: ReactNode
 }
 
-// HACK: need to grab safe area from native device (swiftui, or whatver android uses)
-
 function BudSafeArea({ children }: Props) {
   return (
-    <view className="flex h-full w-full bg-palette-background pt-16 pb-8">
+    <view className="safe-area-view flex h-full w-full bg-palette-background">
       {children}
     </view>
   )
